a96ebbb6e40c645dbc9e1da0dd6c2785a509ab0e
Jan Willem Raats <jw.raats@gmail.com>
2015-11-01 10:24:01 +0100

    Now its working on localhost but on the senzingyou it dont work need to check this at home

--- app/src/main/java/nl/jackevers/jwraats/websocket/MainActivity.java
+++ app/src/main/java/nl/jackevers/jwraats/websocket/MainActivity.java
@@ -5,45 +5,28 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
 import java.net.URISyntaxException;
 
-import io.socket.client.Manager;
-import io.socket.emitter.Emitter;
-import io.socket.client.IO;
-import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    private Socket mSocket;
-    {
-        try {
-            mSocket = IO.socket("http://10.0.2.2:1337");
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private WebSocketClient mWebSocketClient;
+    private String orientation;
+    private boolean connection = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                System.out.println("Connected.."+args[0]);
-            }
-        }).on("text", new Emitter.Listener() {
-            @Override
-            public void call(final Object... args) {
-                System.out.println("Something happend" + args[0]);
-            }
-        });
-        mSocket.connect();
-
+        connectWebSocket();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mSocket.disconnect();
     }
 
     @Override
@@ -52,12 +35,71 @@ public class MainActivity extends AppCompatActivity {
 
         // Checks the orientation of the screen
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            mSocket.send("landscape");
-            getWindow().getDecorView().setBackgroundColor(Color.RED);
-
+            this.orientation = "landscape";
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            mSocket.send("portrait");
-            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+            this.orientation = "portrait";
         }
+        if(this.connection) {
+            mWebSocketClient.send(this.orientation);
+        }
+    }
+
+    private void changeColour(String message){
+        if (message.equals("isGreen: 'true'")) {
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+        } else if(message.equals("isGreen: 'false'")){
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        }
+    }
+
+    private void sendCurrentOrientation(){
+        if(connection) {
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                mWebSocketClient.send("landscape");
+            } else {
+                mWebSocketClient.send("portrait");
+            }
+        }
+    }
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("wss://www.senzingyou.nl:1337");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+        mWebSocketClient = new WebSocketClient(uri, new Draft_17()) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                connection = true;
+                sendCurrentOrientation();
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                changeColour(message);
+
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                connection = false;
+                System.out.println("Closed: "+s);
+                getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                System.out.println("Error: "+e.toString());
+                getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+            }
+        };
+        mWebSocketClient.connect();
+        sendCurrentOrientation();
     }
 }
